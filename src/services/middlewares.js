6fa043b18f52f9863364de0b2d3629f750816cdc
jayeen28 <jayeen6783@gmail.com>
2023-10-14 19:05:10 +0600

    api error middleware update

--- src/services/middlewares.js
+++ src/services/middlewares.js
@@ -13,15 +13,26 @@ function errorMiddleware({ apiErrorPath }) {
       `Body: ${JSON.stringify(body)}\n` +
       `${err.stack}\n`;
 
-    fs.appendFile(apiErrorPath, logMessage, (fileErr) => {
+    // Read the existing content of the file
+    fs.readFile(apiErrorPath, 'utf8', (fileErr, data) => {
       if (fileErr) {
-        console.error('Error logging to file:', fileErr);
+        console.error('Error reading api error file:', fileErr);
+      } else {
+        // Prepend the new log message to the existing content
+        const updatedContent = logMessage + data;
+
+        // Write the updated content back to the file
+        fs.writeFile(apiErrorPath, updatedContent, (writeErr) => {
+          if (writeErr) {
+            console.error('Error writing to api error file:', writeErr);
+          }
+        });
       }
     });
 
     // Send a generic error response to the client
     return res.status(500).send({ message: 'Something went wrong' });
-  }
+  };
 }
 
 module.exports = { errorMiddleware };
